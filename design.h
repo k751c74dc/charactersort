159b9c25f9884d71bd9c8a62ee16387ca7263de7
Kitajima <k751c74dc@outlook.com>
2016-02-02 13:03:22 +0900

    [add] design.hを追加
    効能:ヘッダ，フッタ情報を追加できる
    ただし未実装

---
+++ design.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int header();
+int footer();
+int err();
+
+int header(){
+
+	return 0;
+}
+
+int footer(){
+
+	return 0;
+}
+
+int err(){
+	printf("Content-type:text/html\n\n");
+	printf("<HTML>\n");
+	printf("<HEAD>\n");
+	printf("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n");
+	printf("<TITLE>Error</TITLE>\n");
+	printf("</HEAD>\n");
+	printf("<BODY>\n");
+	printf("error!");
+	printf("</BODY>\n");
+	printf("</HTML>\n");
+	return 0;
+}
