29acd7f7b7e94e713e71bd09353ea760913c57b1
Kitajima <k751c74dc@outlook.com>
2016-02-02 17:29:31 +0900

    [add] Cookieにamount(データ個数)を追加
    追加でインデント補正掛けました

--- cookie.h
+++ cookie.h
@@ -7,6 +7,7 @@ typedef struct {
 	char name[MAX_REGISTER][MAX_CHAR * 3 + 1]; /** register name **/
 	int registeredLink[MAX_REGISTER][MAX_REGISTER]; /** link State **/
 	int ranking[MAX_REGISTER]; /** ranking **/
+	int amount;
 } Cookie;
 
 int init_cookie(Cookie *);
@@ -17,27 +18,28 @@ int init_cookie(Cookie *ck) {
 	int i, j;
 
 	for (i = 0; i < MAX_REGISTER; i++) {
-		strncpy(ck->name[i],"A",MAX_CHAR * 3 + 1);
-		for(j = 0; j < MAX_REGISTER; j++){
+		strncpy(ck->name[i], "A", MAX_CHAR * 3 + 1);
+		for (j = 0; j < MAX_REGISTER; j++) {
 			ck->registeredLink[i][j] = 0;
 		}
 		ck->ranking[i] = -1;
 	}
+	amount = 0;
 
 	return 0;
 }
 
 /** reference at webings.net/c/cookie/ **/
 int get_cookie(Cookie *ck) {
-	int i ,j;
+	int i, j;
 	char cmpString[MAX_CHAR];
 	char ctmp[MAX_CHAR];
 	char *strck, *tp, *tp2;
 
-	strck = (char *)malloc(sizeof(getenv("HTTP_COOKIE")));
+	strck = (char *) malloc(sizeof(getenv("HTTP_COOKIE")));
 
 	if ((strck = getenv("HTTP_COOKIE")) != NULL) {
-		strcat(strck,"; ");
+		strcat(strck, "; ");
 		tp = strtok(strck, "=; ");
 		do {
 			for (i = 0; i < MAX_REGISTER; i++) {
@@ -50,8 +52,8 @@ int get_cookie(Cookie *ck) {
 					tp = strtok(NULL, "=; ");
 				}
 			}
-			for(i = 0; i < MAX_REGISTER; i++){
-				for(j = 0; j < MAX_REGISTER; j++){
+			for (i = 0; i < MAX_REGISTER; i++) {
+				for (j = 0; j < MAX_REGISTER; j++) {
 					sprintf(cmpString, "registeredLink[%d][%d]", i, j);
 					if (strcmp(tp, cmpString) == 0) {
 						tp = strtok(NULL, "=; ");
@@ -62,7 +64,7 @@ int get_cookie(Cookie *ck) {
 					}
 				}
 			}
-			for(i = 0; i < MAX_REGISTER; i++){
+			for (i = 0; i < MAX_REGISTER; i++) {
 				sprintf(cmpString, "ranking[%d]", i);
 				if (strcmp(tp, cmpString) == 0) {
 					tp = strtok(NULL, "=; ");
@@ -72,6 +74,13 @@ int get_cookie(Cookie *ck) {
 					tp = strtok(NULL, "=; ");
 				}
 			}
+			if (strcmp(tp, "amount") == 0) {
+				tp = strtok(NULL, "=; ");
+				strcpy(ctmp, tp);
+				ck->amount = atoi(ctmp);
+				tp = strtok(NULL, "=; ");
+			}
+
 		} while ((tp = strtok(NULL, "=; ")) != NULL);
 	}
 
@@ -86,16 +95,19 @@ int set_cookie(Cookie *ck) {
 		printf("name[%d]=%s;\n", i, ck->name[i]);
 	}
 	for (i = 0; i < MAX_REGISTER; i++) {
-		for(j = 0; j < MAX_REGISTER; j++){
+		for (j = 0; j < MAX_REGISTER; j++) {
 			printf("Set-Cookie: ");
-			printf("registeredLink[%d][%d]=%d;\n",i,j, ck->registeredLink[i][j]);
+			printf("registeredLink[%d][%d]=%d;\n", i, j,
+				ck->registeredLink[i][j]);
 		}
 
 	}
 	for (i = 0; i < MAX_REGISTER; i++) {
 		printf("Set-Cookie: ");
-		printf("ranking[%d]=%d;\n", i,ck->ranking[i]);
+		printf("ranking[%d]=%d;\n", i, ck->ranking[i]);
 	}
+	printf("Set-Cookie: ");
+	printf("amount=%d;\n", ck->amount);
 	printf("\n");
 
 	return 0;
