076d450f7658dce379ff0e10f73b1818064ab5c9
Kitajima <k751c74dc@outlook.com>
2016-02-03 02:11:51 +0900

    [fix] インデント補正をした

--- compare.c
+++ compare.c
@@ -2,12 +2,12 @@
 #include "cookie.h"
 #include "design.h"
 
-void main(int argc, char *argv[]){
-	int i,len;
+void main(int argc, char *argv[]) {
+	int i, len;
 	char c;
 	Cookie *ck;
 
-	ck = (Cookie *)malloc(sizeof(Cookie));
+	ck = (Cookie *) malloc(sizeof(Cookie));
 
 	init_cookie(ck);
 
@@ -19,14 +19,13 @@ void main(int argc, char *argv[]){
 	/** フォームデータをget **/
 	/** もしなかったら(初めの状態)，最初ってこと **/
 	char *pBuf;
-	pBuf = (char *)malloc(len + 1);
+	pBuf = (char *) malloc(len + 1);
 	if (pBuf == NULL) {
 		er();
 		return;
 	}
-	 /**データを受け取る。**/
+	/**データを受け取る。**/
 	//このとき改行文字等は除外しないと後で思わぬエラーが生じる
-
 	for (i = 0; (c = getchar()) != EOF; i++) {
 		if (c != '\r' && c != '\n')
 			pBuf[i] = c;
@@ -40,30 +39,34 @@ void main(int argc, char *argv[]){
 	char aa[100];
 	int left;
 	getstring(pBuf, "LEFT", aa);
-	left =atoi(aa);
+	left = atoi(aa);
 	int right;
 	getstring(pBuf, "RIGHT", aa);
-	right =atoi(aa);
+	right = atoi(aa);
 	int row;
 	getstring(pBuf, "ROW", aa);
-	row =atoi(aa);
+	row = atoi(aa);
 	int column;
 	getstring(pBuf, "COLUMN", aa);
-	column =atoi(aa);
-
+	column = atoi(aa);
 
 	int j;
 	/** フォームデータからCookie(データ構造)を上書きする **/
-	if(column!=row){
-		if(left==1)ck->registeredLink[column][row]=1;
-		if(right==1)ck->registeredLink[row][column]=1;
+	if (column != row) {
+		if (left == 1)
+			ck->registeredLink[column][row] = 1;
+		if (right == 1)
+			ck->registeredLink[row][column] = 1;
 	}
-	int select=0;
-	if(left==1||right==1)row++;
+	int select = 0;
+	if (left == 1 || right == 1)
+		row++;
 
-	if(column==row){
-		row=0;column++;
-		if(column>=ck->amount)select=1;
+	if (column == row) {
+		row = 0;
+		column++;
+		if (column >= ck->amount)
+			select = 1;
 	}
 
 	printf("Content-type:text/html\n");
@@ -72,7 +75,7 @@ void main(int argc, char *argv[]){
 	printf("\n");
 
 	/** 全て埋まったら，sort.cにリダイレクト **/
-	if(select == 1){
+	if (select == 1) {
 		printf("<HTML>\n");
 		printf("<HEAD>\n");
 		printf("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
@@ -80,64 +83,64 @@ void main(int argc, char *argv[]){
 		printf("<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0; URL=sort.cgi \" />");
 		printf("</HEAD>\n");
 		printf("</HTML>\n");
-	}else{
+	} else {
 		printf("<HTML>\n");
 		printf("<HEAD>\n");
-		printf("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+		printf(
+				"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
 		printf("<TITLE>どっちがいい?</TITLE>\n");
 		printf("</HEAD>\n");
 		printf("<BODY>\n");
 		header();
 
-	/** 隠しフォーム形式で 比較を **/
-		printf("[%d][%d]left:%d right:%d<p>",column,row,left,right);
+		/** 隠しフォーム形式で 比較を **/
+		printf("[%d][%d]left:%d right:%d<p>", column, row, left, right);
 		printf("<table><tr>");
 
 		char Lname[100];
-		strcpy(Lname,ck->name[column]);
-		url_decode(Lname,strlen(Lname));
+		strcpy(Lname, ck->name[column]);
+		url_decode(Lname, strlen(Lname));
 		printf("<td><FORM METHOD=\"POST\" ACTION=\"compare.cgi\">");
 		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"LEFT\" value=\"1\">");
 		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"RIGHT\" value=\"0\">");
-		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"COLUMN\" value=\"%d\">",column);
-		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"ROW\" value=\"%d\">",row);
-		printf("<INPUT TYPE=\"SUBMIT\" style=\"width:120px; height:120px;\" VALUE=\"%s\">",Lname );
+		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"COLUMN\" value=\"%d\">", column);
+		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"ROW\" value=\"%d\">", row);
+		printf("<INPUT TYPE=\"SUBMIT\" style=\"width:120px; height:120px;\" VALUE=\"%s\">",Lname);
 		printf("</FORM></td>");
-
 
 		printf("<td><FORM METHOD=\"POST\" ACTION=\"compare.cgi\">");
 		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"LEFT\" value=\"1\">");
 		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"RIGHT\" value=\"1\">");
-		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"COLUMN\" value=\"%d\">",column);
-		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"ROW\" value=\"%d\">",row);
+		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"COLUMN\" value=\"%d\">", column);
+		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"ROW\" value=\"%d\">", row);
 		/*printf("<INPUT TYPE=\"SUBMIT\" VALUE=\"引き分け\"><P>");*/
 		printf("</FORM></td>");
 
 		printf("<td><FORM METHOD=\"POST\" ACTION=\"compare.cgi\">");
 		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"LEFT\" value=\"2\">");
 		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"RIGHT\" value=\"2\">");
-		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"COLUMN\" value=\"%d\">",column);
-		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"ROW\" value=\"%d\">",row);
+		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"COLUMN\" value=\"%d\">", column);
+		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"ROW\" value=\"%d\">", row);
 		/*printf("<INPUT TYPE=\"SUBMIT\" VALUE=\"どちらも知らない\"><P>");*/
 		printf("</FORM></td>");
 
 		char Rname[100];
-		strcpy(Rname,ck->name[row]);
-		url_decode(Rname,strlen(Rname));
+		strcpy(Rname, ck->name[row]);
+		url_decode(Rname, strlen(Rname));
 		printf("<td><FORM METHOD=\"POST\" ACTION=\"compare.cgi\">");
 		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"LEFT\" value=\"0\">");
 		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"RIGHT\" value=\"1\">");
-		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"COLUMN\" value=\"%d\">",column);
-		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"ROW\" value=\"%d\">",row);
-		printf("<INPUT TYPE=\"SUBMIT\" style=\"width:120px; height:120px;\" VALUE=\"%s\"><P>", Rname);
+		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"COLUMN\" value=\"%d\">", column);
+		printf("<INPUT TYPE=\"HIDDEN\" NAME=\"ROW\" value=\"%d\">", row);
+		printf("<INPUT TYPE=\"SUBMIT\" style=\"width:120px; height:120px;\" VALUE=\"%s\"><P>",Rname);
 		printf("</FORM></td>");
 		printf("</tr></table>");
 		/** 自身(compare.cgi)に送信 **/
 
-		for(i=0;i<ck->amount;i++){
+		for (i = 0; i < ck->amount; i++) {
 			int j;
-			for(j=0;j<ck->amount;j++){
-				printf("|[%d][%d]=%f|",i,j,ck->registeredLink[i][j]);
+			for (j = 0; j < ck->amount; j++) {
+				printf("|[%d][%d]=%f|", i, j, ck->registeredLink[i][j]);
 			}
 			printf("<p>");
 
@@ -150,16 +153,15 @@ void main(int argc, char *argv[]){
 	return;
 }
 
-int getstring(char *src, char *element, char *dest)
-{
+int getstring(char *src, char *element, char *dest) {
 	int len;
 	char *data, *amp;
 	char *temp;
 
-	len = (int)strlen(src) + 1;
-	temp = (char *)malloc(len);
+	len = (int) strlen(src) + 1;
+	temp = (char *) malloc(len);
 
-	len = (int)strlen(src);
+	len = (int) strlen(src);
 	if (len >= 1024) {
 		er();
 		free(temp);
@@ -169,13 +171,13 @@ int getstring(char *src, char *element, char *dest)
 	strcpy(temp, src);
 
 	data = strstr(temp, element);
-	if(data == NULL) {
+	if (data == NULL) {
 		strcpy(dest, "no element");
 		free(temp);
 		return -2;
 	}
 
-	len = (int)strlen(element) + 1;
+	len = (int) strlen(element) + 1;
 
 	amp = strstr(data, "&");
 	if (amp == NULL) {
@@ -188,7 +190,7 @@ int getstring(char *src, char *element, char *dest)
 		free(temp);
 		return 1;
 	}
-	data[(int)(amp-data)] = '\0';
+	data[(int) (amp - data)] = '\0';
 	strcpy(dest, data + len);
 
 	if (dest[0] == '\0') {
@@ -200,19 +202,15 @@ int getstring(char *src, char *element, char *dest)
 	free(temp);
 	return 0;
 }
-char *strstr(
-	const char *string,
-	const char *strCharSet
-	);
-int er()
-{
+char *strstr(const char *string, const char *strCharSet);
+int er() {
 	printf("Content-type:text/html\n\n");
 	printf("<HTML>\n");
 	printf("<HEAD>\n");
-	printf("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=Shift_JIS\">\n");
+	printf("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n");
 	printf("<TITLE>Error</TITLE>\n");
 	printf("</HEAD>\n");
-	printf("<BODY BGCOLOR=\"#FF0000\">\n");
+	printf("<BODY>\n");
 	printf("error!");
 	printf("</BODY>\n");
 	printf("</HTML>\n");
