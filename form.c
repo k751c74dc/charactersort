29c96e78926aad34e03531195101bbb2ab05c82f
Shunya Kumagai <syunya.yankiti@gmail.com>
2016-02-02 14:41:36 +0900

    [modified] form.cの修正
    効能:convert.cへname[*]として受け渡すようにした

--- form.c
+++ form.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include <design.h>
+#include "design.h"
+#include "define.h"
 
 void main(int argc, char *argv[]){
+	int i;
 	printf("Content-type:text/html\n\n");
 
 	printf("<HTML>\n");
@@ -13,6 +15,20 @@ void main(int argc, char *argv[]){
 	header();
 
 	/** ここにフォームを入力 **/
+	printf("<form action=\"convert.cgi\" method=\"post\">\n");
+
+	for ( i = 0; i < MAX_REGISTER; i++){
+		printf("<p>");
+		printf("データ%d:<input type=\"text\" name=\"name[%d]\" size=\"40\">\n",i,i);
+		printf("</p>");
+	}
+
+	printf("<p>");
+	printf("<input type=\"submit\" value=\"送信\"><input type=\"reset\" value=\"リセット\">\n");
+	printf("</p>");
+
+	printf("</form>");
+	/** フォームここまで **/
 
 	footer();
 	printf("</BODY>\n");
